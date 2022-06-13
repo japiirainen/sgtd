bd695843a5b4826b7a6708177f7468ae86ff01a2
Joona Piirainen <joona.piirainen@gmail.com>
2022-06-13 18:28:54 +0300

    Chore: Initialize project

---
+++ env/dev/clj/japiirainen/sgtd/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns japiirainen.sgtd.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ test/clj/japiirainen/sgtd/core_test.clj
@@ -0,0 +1,8 @@
+(ns japiirainen.sgtd.core-test
+  (:require
+    [japiirainen.sgtd.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
